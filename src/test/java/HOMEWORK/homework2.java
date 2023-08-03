723b3b2252cbc59de6892f83fcbc1edca011cf16
Anthony Tsai <116094020+Tsai-Anthony@users.noreply.github.com>
2023-08-03 18:44:15 -0400

    Update homework2.java

--- src/test/java/HOMEWORK/homework2.java
+++ src/test/java/HOMEWORK/homework2.java
@@ -1,5 +1,25 @@
 package HOMEWORK;
 
 public class homework2 {
+//	create a script that navigates to https://boratech-practice-app.onrender.com/
+//	have it log in
+//	navigate to add experience
+//	create 2 exceptions
+//	use try and catch 
+//	also include finally
+	
+	
+//	syntax
 
+//	try {
+//		  //  Block of code to try
+//		}
+//		catch(Exception e) {
+//		  //  Block of code to handle errors
+//		}
+
+	
+	
+	
+}
 }
