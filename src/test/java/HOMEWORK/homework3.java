f6c622bc314cfe7cc69a3364b0566b0e365c8fd4
Anthony Tsai <116094020+Tsai-Anthony@users.noreply.github.com>
2023-08-03 18:44:36 -0400

    Update homework3.java

--- src/test/java/HOMEWORK/homework3.java
+++ src/test/java/HOMEWORK/homework3.java
@@ -1,5 +1,10 @@
 package HOMEWORK;
 
 public class homework3 {
-
+//	create a script that navigates to https://boratech-practice-app.onrender.com/
+// have it login and go to add experience
+// create a unhappy path
+// check for single error (missing job title)
+// dont forget try / catch!
+}
 }
