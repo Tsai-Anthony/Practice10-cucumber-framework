ab46e04c73fd4b7dc28d9a37d2584d8f50f9466e
Anthony Tsai <116094020+Tsai-Anthony@users.noreply.github.com>
2023-08-03 18:43:44 -0400

    Update homework1.java

--- src/test/java/HOMEWORK/homework1.java
+++ src/test/java/HOMEWORK/homework1.java
@@ -1,5 +1,12 @@
 package HOMEWORK;
 
 public class homework1 {
+//Create a script that navigates to https://boratech-practice-app.onrender.com/
+// it must login
+// click on add experience
+//	navigate back to dashboard
+//	click on add education
 
+	
+	//also create string variables to practice organization!
 }
