a92540a396b5cfdc84fac785ab2a13dde96d9b2b
Anthony Tsai <116094020+Tsai-Anthony@users.noreply.github.com>
2023-08-03 18:44:51 -0400

    Update homework4.java

--- src/test/java/HOMEWORK/homework4.java
+++ src/test/java/HOMEWORK/homework4.java
@@ -1,5 +1,10 @@
 package HOMEWORK;
 
 public class homework4 {
-
+//	create a script that navigates to https://boratech-practice-app.onrender.com/
+// have it login and go to add experience
+// create a unhappy path
+// check for multiple error (missing job title and missing comany)
+// dont forget try / catch!
+}
 }
